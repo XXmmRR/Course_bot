626c4fa70acf3a269317ff1f968a3cc2e74577ef
q <Xorex50@protonmail.com>
2023-12-12 20:24:15 +0500

    add follow menu

--- gspread_utils/text/texts.py
+++ gspread_utils/text/texts.py
@@ -5,6 +5,9 @@ def get_text(sheet=sheet, col_id=3):
     texts = []
     keyboards = []
     temp_keyboard = []
+    temp_actions = []
+    actions = []
+
     old_count = 0
     current_count = 0
     for i in sheet.get_all_values():
@@ -21,5 +24,16 @@ def get_text(sheet=sheet, col_id=3):
                     keyboards.append(temp_keyboard.copy())
                     temp_keyboard.clear()
                     temp_keyboard.append(i[3])
+        if i[0].startswith('Следствие выбора'):
+            if i[3] != '' and i[3] != '-':
+                if current_count - 1 == old_count:
+                    temp_actions.append(i[3])
+                else:
+                    old_count += 1
+                    actions.append(temp_actions.copy())
+                    temp_actions.clear()
+                    temp_actions.append(i[3])
+
+
 
     return texts, keyboards

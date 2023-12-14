4c517636634fb798f3295387b5b352bdedeb46e1
q <Xorex50@protonmail.com>
2023-12-14 22:18:57 +0500

    fix google docs alghoritm

--- gspread_utils/text/texts.py
+++ gspread_utils/text/texts.py
@@ -2,38 +2,35 @@ from gspread_utils.google_client import sheet
 
 
 def get_text(sheet=sheet, col_id=3):
-    texts = []
-    keyboards = []
-    temp_keyboard = []
-    temp_actions = []
-    actions = []
+    texts = []          # Список для текстов вопросов
+    keyboards = []      # Список для наборов клавиатур (ответов)
+    actions = []        # Список для наборов действий, соответствующих каждому набору клавиатур
 
-    old_count = 0
-    current_count = 0
-    for i in sheet.get_all_values():
-        if i[0] == 'Вопрос ':
-            current_count += 1
-            if i[3] != '' and i[3] != '-':
-                texts.append(i[3])
-        if i[0].startswith('Ответ'):
-            if i[3] != '' and i[3] != '-':
-                if current_count - 1 == old_count:
-                    temp_keyboard.append(i[3])
-                else:
-                    old_count += 1
+    temp_keyboard = []  # Временный список для сбора ответов текущего вопроса
+    temp_actions = []   # Временный список для сбора действий текущего вопроса
+    current_index = 1
+
+    for row in sheet.get_all_values():
+        # Проверяем, является ли строка началом нового вопроса
+        if row[3] != '' and row[3] != '-' or row[0].isdigit():
+            if row[0] == 'Вопрос ':
+                texts.append(row[3])
+            elif row[0].startswith('Ответ') and texts:
+                temp_keyboard.append(row[col_id])
+
+            # Если строка является следствием выбора, собираем её во временный список действий
+            elif row[0].startswith('Следствие выбора') and texts:
+                temp_actions.append(row[col_id])
+            if row[0].isdigit() and int(row[0]) != current_index:
+                if temp_keyboard:
                     keyboards.append(temp_keyboard.copy())
-                    temp_keyboard.clear()
-                    temp_keyboard.append(i[3])
-        if i[0].startswith('Следствие выбора'):
-            if i[3] != '' and i[3] != '-':
-                if current_count - 1 == old_count:
-                    temp_actions.append(i[3])
-                else:
-                    old_count += 1
+                if temp_actions:
                     actions.append(temp_actions.copy())
-                    temp_actions.clear()
-                    temp_actions.append(i[3])
+                temp_actions.clear()
+                temp_keyboard.clear()
+                current_index += 1
+
+    return texts, keyboards, actions
 
 
-
-    return texts, keyboards
+print(get_text())

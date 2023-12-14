280d199ef9a633f56dd12864f69b69b4b8c09a55
q <Xorex50@protonmail.com>
2023-12-14 22:46:39 +0500

    Оно работает

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -5,7 +5,8 @@ from keyboards.main.main_keyboard import get_main_menu_keyboard, get_keyboard_by
 from filters.text_filter import Textfilter
 from gspread_utils.buttons.get_main_buttons import get_main_keyboard_fields
 from gspread_utils.text.texts import get_text
-
+from states.menu_routing_states import FollowHandlers
+from aiogram.fsm.context import FSMContext
 
 menu_router = Router(name='menu router')
 
@@ -25,5 +26,48 @@ async def watch_video(message: types.Message):
 
 
 @menu_router.message(Textfilter(fiedls[0]))
-async def about_company_main(message: types.Message):
+async def about_company_main(message: types.Message, state: FSMContext):
     await message.answer(company_texts[0][0], reply_markup=get_keyboard_by_list(keyboard_list=company_texts[1][0]))
+    await state.set_state(FollowHandlers.first_handler)
+
+
+@menu_router.message(FollowHandlers.first_handler)
+async def handle_message(message: types.Message, state: FSMContext):
+    user_data = await state.get_data()
+    current_index = user_data.get('current_index', 0)  # Получаем текущий индекс, начинаем с 0
+
+    texts, keyboards, actions = get_text()  # Получаем данные
+
+    # Нормализуем текст сообщения, удаляем пробелы и приводим к нижнему регистру
+    normalized_message_text = message.text.strip().lower()
+
+    # Нормализуем варианты клавиатуры, тоже удаляем пробелы и приводим к нижнему регистру
+    normalized_keyboard_options = [option.strip().lower() for option in keyboards[current_index]]
+
+    # Проверяем, совпадает ли нормализованный текст сообщения с одной из нормализованных опций клавиатуры
+    if normalized_message_text in normalized_keyboard_options:
+        # Получаем индекс выбранной опции ответа
+        choice_index = normalized_keyboard_options.index(normalized_message_text)
+        # Получаем соответствующее действие
+        action = actions[current_index][choice_index]
+
+        if action == 'Следующий вопрос':
+            # Убедитесь, что индекс не выходит за рамки списка
+            if current_index + 1 < len(texts):
+                current_index += 1  # Переходим к следующему вопросу
+            else:
+                # Здесь может быть ваша логика для завершения диалога или цикла вопросов
+                await message.answer("Вы достигли конца диалога.")
+        elif action == 'В меню':
+            await read_text(message)
+            await state.clear()
+            return
+
+        # Сохраняем обновлённый индекс текущего вопроса в состоянии
+        await state.update_data(current_index=current_index)
+
+        # Отправляем следующий текст
+        if current_index < len(texts):
+            await message.answer(texts[current_index], reply_markup=get_keyboard_by_list(keyboards[current_index]))
+    else:
+        await message.answer("Пожалуйста, выберите один из предложенных вариантов ответов.")
---
+++ states/menu_routing_states.py
@@ -0,0 +1,5 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class FollowHandlers(StatesGroup):
+    first_handler = State()
